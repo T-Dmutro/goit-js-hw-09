f15625cb250e29828dd9462bb0b6a781c80df63c
T-Dmutro <90419458+T-Dmutro@users.noreply.github.com>
2023-02-24 17:02:02 +0200

    task 1 full

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
 
+btnStart = document.querySelector('[data-start]');
+btnStop = document.querySelector('[data-stop]');
+// console.log(btnStart, btnStop)
+let activStatus = false;
+ btnStart.addEventListener("click", handBtnClickStart);
+ btnStop.addEventListener("click", handBtnClickStop)
+
+
+
+ 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+function handBtnClickStart ( ){
+    if (activStatus ){
+        return
+    }
+    timerId = setInterval(()=> {
+        activStatus = true;
+        document.body.style.background = getRandomHexColor() ;
+        }, 1000);
+        
+console.log( 'click start')
+};
+
+function handBtnClickStop ( ) {
+     clearInterval(timerId),
+    console.log('stop interval'),
+    activStatus = false;
+};
