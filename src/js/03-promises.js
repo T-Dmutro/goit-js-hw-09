359e3ae5a85ebf3f4348d4cd66af447019ed932e
T-Dmutro <90419458+T-Dmutro@users.noreply.github.com>
2023-03-01 12:49:57 +0200

    promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve({ position, delay });
+    } else {
+      reject({ position, delay });
+    }
+  });
+}
+
+const form = document.querySelector('.form');
+form.addEventListener("submit", onSubmitForm);
+
+function onSubmitForm(element) {
+  element.preventDefault();
+  let { delay, step, amount } = element.currentTarget;
+
+  amount = Number(amount.value);
+  step = Number(step.value);
+  delay = Number(delay.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(({ i, delay }) => {
+        setTimeout(() => {
+          Notify.success(`✅ Fulfilled promise ${i} in ${delay}ms`);
+        }, delay);
+      })
+      .catch(({ i, delay }) => {
+        setTimeout(() => {
+          Notify.failure(`❌ Rejected promise ${i} in ${delay}ms`);
+        }, delay);
+      });
+    delay += step;
   }
+
 }
