c0c31908dfdbe7eaa050611ac31e14cd2f0e3500
T-Dmutro <90419458+T-Dmutro@users.noreply.github.com>
2023-03-02 11:33:30 +0000

    Deploying to gh-pages from @ T-Dmutro/goit-js-hw-09@0947aefa14f5b29e4ffe58c72dfbc8eaffe517c3 🚀

---
+++ 01-color-switcher.bc5faa9d.js
@@ -0,0 +1,2 @@
+!function(){btnStart=document.querySelector("[data-start]"),btnStop=document.querySelector("[data-stop]");var t=!1;btnStart.addEventListener("click",(function(){if(t)return;timerId=setInterval((function(){t=!0,document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),console.log("click start")})),btnStop.addEventListener("click",(function(){clearInterval(timerId),console.log("stop interval"),t=!1}))}();
+//# sourceMappingURL=01-color-switcher.bc5faa9d.js.map
